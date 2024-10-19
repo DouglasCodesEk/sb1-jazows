d8f6ae8d6c1168228412d318e24b8baa8fc416ed
DouglasCodesEk <douglasek@proton.me>
2024-10-19 15:05:13 +0200

    Game deployment

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/DouglasCodesEk/sb1-jazows/src/app/layout.tsx": [
+      "/home/DouglasCodesEk/sb1-jazows/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"src/app/layout.tsx\",\"variableName\":\"inter\"}",
+      "/home/DouglasCodesEk/sb1-jazows/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/DouglasCodesEk/sb1-jazows/src/app/page": [
+      "/home/DouglasCodesEk/sb1-jazows/src/app/globals.css",
+      "/home/DouglasCodesEk/sb1-jazows/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"src/app/layout.tsx\",\"variableName\":\"inter\"}"
+    ]
+  }
+}
